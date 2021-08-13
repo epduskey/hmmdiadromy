456acad93766f326b23896039922647543ec8fe3
Elizabeth Duskey <elizabeth.duskey@slu.se>
2021-08-13 15:54:19 -0400

    Delete depend.R

--- depend.R
+++
@@ -1,8 +0,0 @@
-# Dependencies
-#install.packages("jagsUI")
-#install.packages("rje")
-#install.packages("HDInterval")
-#install.packages("vioplot")
-
-# Set path to HMM
-mypath = "/Users/epdus/OneDrive/School/Research/"
