15119f19b3614ba344d1fc96868526fff5c784f8
Elizabeth Duskey <elizabeth.duskey@slu.se>
2022-10-24 11:10:58 -0400

    Add files via upload

--- Code/depend.R
+++ Code/depend.R
@@ -2,6 +2,7 @@
 #install.packages("jagsUI")
 #install.packages("mgcv")
 #install.packages("CARBayesST")
+#install.packages("mclogit")
 #install.packages("rje")
 #install.packages("HDInterval")
 #install.packages("vioplot")
